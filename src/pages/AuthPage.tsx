ad0c436d11862a3b3b0f67f6bd70675b2e5ab074
hurkaliuknikita-svg <hurkaliuknikita@gmail.com>
2025-12-26 14:56:43 +0200

    fix description on auth page

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -8,14 +8,16 @@ export default function AuthPage() {
   return (
     <div className={css.auth_card}>
       <div className={css.left}>
-        <h1>Meeting Room Booking App</h1>
-        <p>Registration and booking management.</p>
+        <h1>Travel planning service</h1>
+        <p>
+          Create trips, organize places, and collaborate with others in one
+          convenient workspace. Plan journeys together, manage details, and keep
+          everything structured from start to finish.
+        </p>
       </div>
       <div className={css.right}>
         <h3>{isSignup ? "Signup" : "Login"}</h3>
-        <AuthForm
-          isSignup={isSignup}
-        />
+        <AuthForm isSignup={isSignup} />
         {!isSignup && (
           <p className={css.bottom}>
             Don't have an account?
